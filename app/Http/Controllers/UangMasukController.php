4439761c36ede1084205b1d60e9deb76c4da58b4
taufiqhida <taufiqh2003@gmaill.com>
2024-12-26 09:09:21 +0700

    Buat storenya di uang masuk

--- app/Http/Controllers/UangMasukController.php
+++ app/Http/Controllers/UangMasukController.php
@@ -13,5 +13,26 @@ class UangMasukController extends Controller
         $income = Income::with(['source'])->get();
         return view('keuangan-uang-masuk', compact('income'));
     }
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'source_id' => 'required|exists:sources,id',
+            'date' => 'required|date',
+            'amount' => 'required|integer',
+            'description' => 'nullable|string'
+        ]);
+
+        $income = Income::create([
+            'name' => $request->name,
+            'source_id' => $request->source_id,
+            'date' => $request->date,
+            'amount' => $request->amount,
+            'description' => $request->description
+        ]);
+
+        return redirect()->back()->with('success', 'Data berhasil disimpan');
+    }
 
 }
