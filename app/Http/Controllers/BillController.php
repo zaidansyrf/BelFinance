7c71c8992f252542b809d0e45872c962295d48d6
taufiqhida <taufiqh2003@gmaill.com>
2024-12-29 20:12:15 +0700

    tambah hapus bill

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -21,6 +21,19 @@ class BillController extends Controller
         Bill::create($request->all());
         return redirect()->route('sumber-keluar.index')->with('success', 'Bill berhasil ditambahkan.');
     }
+    public function destroy(Bill $sumber_keluar)
+    {
+        // Pastikan data ditemukan
+        if (!$sumber_keluar) {
+            return redirect()->route('sumber-keluar.index')->with('error', 'Data tidak ditemukan.');
+        }
+    
+        // Hapus data
+        $sumber_keluar->delete();
+    
+        return redirect()->route('sumber-keluar.index')->with('success', 'Data berhasil dihapus.');
+    }
+       
 
 }
 
