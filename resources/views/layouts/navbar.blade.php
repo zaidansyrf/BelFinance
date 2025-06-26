6b825ce3618d12f88814135397e864ab356cd54a
zaidansyrf <zaidansyarif47@gmail.com>
2025-06-26 22:17:47 +0700

    minor changes

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -54,11 +54,9 @@
                 <div class="avatar">
                     <div class="w-12 h-12 rounded-full">
                         <a class="inline-flex items-center justify-center p-2 hover:text-[#000000] mt-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5"
-                                stroke="currentColor" class="h-6 w-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
+                            <img class="h-12 w-12 rounded-full"
+                                src="https://ui-avatars.com/api/?name={{ urlencode(auth()->user()->name) }}&background=68585&color=fff"
+                                alt="Profile photo">
                         </a>
                     </div>
                 </div>
